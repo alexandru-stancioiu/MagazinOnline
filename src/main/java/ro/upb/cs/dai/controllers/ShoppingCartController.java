fc8279ada75171158d06ca600a1d6e0b201168dc
Alexandru Stancioiu <stancioi@adobe.com>
2017-01-08 00:36:44 +0200

    remove imports

--- src/main/java/ro/upb/cs/dai/controllers/ShoppingCartController.java
+++ src/main/java/ro/upb/cs/dai/controllers/ShoppingCartController.java
@@ -1,14 +1,11 @@
 package ro.upb.cs.dai.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import ro.upb.cs.dai.entities.Product;
-import ro.upb.cs.dai.entities.Role;
-import ro.upb.cs.dai.model.ShoppingCart;
 import ro.upb.cs.dai.service.ProductService;
 import ro.upb.cs.dai.service.ShoppingCartService;
 
